3e1e0ab4219eab3fc4fda1e0ff58984bdbb0d758
Twsila <rahman.shagawi@outlook.com>
2024-11-12 20:33:54 +0300

    Add DemoOperator Class

---
+++ DemoLogicalOperator.java
@@ -0,0 +1,14 @@
+public class DemoLogicalOperator {
+
+  public static void main(String[] args) {
+    int x = 7;
+    int y = 5;
+    int a = 5;
+    int b = 9;
+
+    boolean result = x > y && a > b;
+    System.out.println(result);
+
+
+  }
+}
