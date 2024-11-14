99c88e410182dc643c1f0bff375a1278012dd0e7
Twsila <128645255+twsila@users.noreply.github.com>
2024-11-14 16:14:49 +0300

    Add Loop Class Update from KFU CODE-2

--- DemoForLoop.java
+++ DemoForLoop.java
@@ -1,7 +1,7 @@
 public class DemoForLoop {
     public static void main(String[] args) {
         for(int i=0;i <=10;i++){
-            System.out.println(i);
+            System.out.println("Maha is mylove.."+i);
         }
     }
 }
