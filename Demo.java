88520996b963c5e6e69f5c7bb1efdf12cb3670c8
Twsila <128645255+twsila@users.noreply.github.com>
2024-11-15 19:59:10 +0300

    Add Nested Loop Class Update from KFU CODE

--- Demo.java
+++ Demo.java
@@ -4,5 +4,21 @@ public class Demo {
     int y = 6;
     boolean result = x<y;
     System.out.println(result);
+    int a = 3;
+
+    int b = 6;
+
+    int result2 = (~a & b) | (a & ~b);
+
+    System.out.println(result2);
+
+    int xx = 5;
+
+    int yy = 10;
+
+    int z = (xx++ > 5 && yy-- < 10) ? xx-- : yy;
+    System.out.println("x :"+xx);
+    System.out.println("y :"+yy);
+    }
   }
-}
+
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This a shared repository for my Java practice
 This line was published by VSCode to GitHub Repo.
 Line added from my PC on KFU CODE
-Today me and Gamal will start planning to Migrate from GCP to AWS
+Today myself and Gamal will start planning to Migrate from GCP to AWS
