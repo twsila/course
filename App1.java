3d221c49ea99f78509a9002ba67bdf47098839c1
Twsila <rahman.shagawi@outlook.com>
2024-11-16 15:13:26 +0300

    Update from Lenovo Laptop

---
+++ App1.java
@@ -0,0 +1,28 @@
+public class App1 {
+  public static void main(String[] args) {
+
+    // int a = 3;
+
+    // int b = 6;
+    // System.out.print("~a & b: ");
+    // System.out.println(~a & b);
+
+    // System.out.print("a & ~b: ");
+    // System.out.println(a & ~b);
+    
+    // int result = (~a & b) | (a & ~b);
+
+    // System.out.println(result);
+
+    int i, j;
+
+    i = 100;
+    
+    j = 300;
+    
+    while(++i < --j);
+    
+    System.out.println(i);
+
+}
+}
